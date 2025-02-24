8b88e9e7dea492898f3877be172c2070fc2c4309
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-02-24 15:50:09 -0300

    exercices 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    let newArr = [];
+    arr.forEach((element) => {
+        if (!num.includes(element)) {
+            newArr.push(element);
+        }
+    });
+    return newArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(integer1, integer2) {
+    if (typeof integer1 !== 'number' || typeof integer2 !== 'number' || integer1 < 0 || integer2 < 0 || !Number.isInteger(integer1) || !Number.isInteger(integer2)) {
+        return 'ERROR';
+    }
+    
+    let sum = 0;
+    let start = integer1 < integer2 ? integer1 : integer2;
+    let end = integer1 > integer2 ? integer1 : integer2;
+    
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
